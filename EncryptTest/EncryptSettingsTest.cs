a2ce1a62d67d058f719c9e67bf750aed1c03fc25
tsntsumi <tsntsumi@tsntsumi.com>
2015-12-02 22:48:38 +0900

    EncryptSettings クラスのユニットテストを追加

--- EncryptTest/EncryptSettingsTest.cs
+++ EncryptTest/EncryptSettingsTest.cs
@@ -1,14 +1,97 @@
 ﻿using NUnit.Framework;
 using System;
+using Encrypt;
 
 namespace EncryptTest
 {
     [TestFixture()]
-    public class Test
+    public class EncryptSettingsTest
     {
-        [Test()]
-        public void TestCase()
+        EncryptSettings settings;
+
+        [SetUp()]
+        public void SetUpSettings()
         {
+            settings = EncryptSettings.Instance;
+        }
+
+        [Test()]
+        public void UniqueInstanceTest()
+        {
+            var moreSettings = EncryptSettings.Instance;
+
+            Assert.AreSame(settings, moreSettings);
+        }
+
+        [Test(), ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThrowArgumentOutOfRangeExceptionWhenBlockSizeIsNot128()
+        {
+            settings.BlockSize = 192;
+        }
+
+        [Test()]
+        public void BlockSizeMustBe128()
+        {
+            try
+            {
+                settings.BlockSize = 128;
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test(), ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThrowArgumentOutOfRangeExceptionWhenKeySizeIsNot128_192_256()
+        {
+            settings.KeySize = 144;
+        }
+
+        [Test()]
+        public void KeySizeCanBeAssigned128_192_256()
+        {
+            try
+            {
+                settings.KeySize = 128;
+                settings.KeySize = 192;
+                settings.KeySize = 256;
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test(), ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThrowArgumentOutOfRangeExceptionWhenSaltSizeIsNotMultipleBy8()
+        {
+            settings.SaltSize = 7;
+        }
+
+        [Test(), ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThrowArgumentOutOfRangeExceptionWhenSaltSizeIsMinus()
+        {
+            settings.SaltSize = -8;
+        }
+
+        [Test(), ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThrowArgumentOutOfRangeExceptionWhenSaltSizeIsZero()
+        {
+            settings.SaltSize = 0;
+        }
+
+        [Test()]
+        public void SaltSizeCanBeAssignValueMultipleBy8()
+        {
+            try
+            {
+                settings.SaltSize = 7 * 8;
+            }
+            catch
+            {
+                Assert.Fail();
+            }
         }
     }
 }
